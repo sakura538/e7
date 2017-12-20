bb9b22cd64a3b7ee93c99e6af4ac4c1b9d7482b5
yijun <yijunyunnan@163.com>
2017-12-20 21:44:50 +0800

    manager

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,8 +1,10 @@
 package cn.e3mall.service;
 
+import cn.e3mall.pojo.EasyUIDatagridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 
 	TbItem getItemById(Long itemId);
+	public EasyUIDatagridResult getItemList(int page, int rows);
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.EasyUIDatagridResult;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 
 @Service
@@ -18,6 +25,19 @@ public class ItemServiceImpl implements ItemService{
 		TbItem item = new TbItem();
 		 item = mapper.selectByPrimaryKey(itemId);
 		return item;
+	}
+	
+	@Override
+	public EasyUIDatagridResult getItemList(int page, int rows){
+		PageHelper.startPage(page, rows);
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = mapper.selectByExample(example);
+		
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		EasyUIDatagridResult result = new EasyUIDatagridResult();
+		result.setRows(list);
+		result.setTotal(pageInfo.getTotal());
+		return result;
 	}
 
 }
